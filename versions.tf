7d704d3e612e4d71c748b1be192cc42a223f03b1
Vinkum003 <61989816+Vinkum003@users.noreply.github.com>
2022-04-22 13:29:23 -0400

    updated backend TF

--- versions.tf
+++ versions.tf
@@ -1,13 +1,14 @@
 terraform {
+  backend "gcs" {
+    bucket  = "demo_terraform_backend"
+    prefix  = "jenkins_pipeline/"
+  } 
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "3.52.0"
     }
-  backend "gcs" {
-    bucket  = "demo_terraform_backend"
-    prefix  = "jenkins_pipeline/"
   }
-  required_version = ">= 0.14"
+    required_version = ">= 0.14"
 }
 
