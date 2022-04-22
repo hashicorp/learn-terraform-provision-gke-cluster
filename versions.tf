3c4a152bc99e7b9de4288b671ab79d78aaf7cc4f
Vinkum003 <61989816+Vinkum003@users.noreply.github.com>
2022-04-22 09:52:03 -0400

    Updating backend conf and terraform state

--- versions.tf
+++ versions.tf
@@ -4,8 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "3.52.0"
     }
+  backend "gcs" {
+    bucket  = "demo_terraform_backend"
+    prefix  = "jenkins_pipeline/"
   }
-
   required_version = ">= 0.14"
 }
 
