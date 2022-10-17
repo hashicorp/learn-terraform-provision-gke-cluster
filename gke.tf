72f5eb6dba5e72338ecd94b0b11596cecbeff0db
Mauricio Alvarez Leon <65101411+BBBmau@users.noreply.github.com>
2022-10-17 11:13:46 -0700

    update version number for ARM64 compatibility (#13)
    * update version number for ARM64 compatibility
    
    * Update Terraform Lock

--- gke.tf
+++ gke.tf
@@ -30,7 +30,7 @@ resource "google_container_cluster" "primary" {
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "${google_container_cluster.primary.name}-node-pool"
+  name       = "${google_container_cluster.primary.name}"
   location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.52.0"
+      version = "4.27.0"
     }
   }
 
